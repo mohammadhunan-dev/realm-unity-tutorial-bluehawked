89917f382d5bbd826c50e594ba33d8f9ba54f6b5
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2021-09-08 14:54:03 -0400

    update bluehawk snippets

--- docs/RealmScripts/PlayerModel.cs
+++ docs/RealmScripts/PlayerModel.cs
@@ -1,75 +1,33 @@
-// :code-block-start: unity-player-model
-// :state-start: final
-// >>>> HERE
-// :state-end: :state-uncomment-start: start
-//// TODO: implement schema
-// :state-uncomment-end:
-// :code-block-end:
-
-
-
 using System.Collections.Generic;
 using MongoDB.Bson;
 using Realms;
 
 // :code-block-start: player-model
 // :state-start: local sync
-public class PlayerModel : RealmObject { 
+public class Player : RealmObject
+{
 
     [PrimaryKey]
     [MapTo("_id")]
     [Required]
-    public string Id { get; set; }
+    public string Id { get; set; } 
 
-    [MapTo("_partition")]
-    [Required]
-    public string Partition { get; set; }
 
-    [MapTo("scores")]
-    public IList<ScoreModel> Scores { get; }
+    [MapTo("stats")]
+    public IList<Stat> Stats { get; }
 
     [MapTo("name")]
     [Required]
     public string Name { get; set; }
 
 }
-// :state-end: :state-uncomment-start: start
-// // TODO: Realm-ify Task model
-// class Task {
-//    var name: String = ""
-//    var statusEnum: TaskStatus = .Open
-// }
-// :state-uncomment-end:
-// :code-block-end:
-
-
-// :code-block-start: task-model
-// :state-start: local sync
-class Task: Object {
-    @Persisted(primaryKey: true) var _id: ObjectId
-    @Persisted var name: String = ""
-    @Persisted var owner: String?
-    @Persisted var status: String = ""
-
-    var statusEnum: TaskStatus {
-        get {
-            return TaskStatus(rawValue: status) ?? .Open
-        }
-        set {
-            status = newValue.rawValue
-        }
-    }
-
-    convenience init(name: String) {
-        self.init()
-        self.name = name
-    }
-}
-// :state-end: :state-uncomment-start: start
-// // TODO: Realm-ify Task model
-// class Task {
-//    var name: String = ""
-//    var statusEnum: TaskStatus = .Open
+// :state-end: 
+// :state-uncomment-start: start
+// // TODO: Realm-ify Player model
+// public class PlayerModel {
+//  public string _id;
+//  public string name
+//  public IList<Stat> stats
 // }
 // :state-uncomment-end:
 // :code-block-end:
